f91504a81af70690d342cfb9c8a906afa89a4b28
Bria Walker <briawalker@brias-mbp.attlocal.net>
2018-02-23 13:28:12 -0600

    updates initial files

---
+++ App/data/friends.js
@@ -0,0 +1,19 @@
+var userInfo = {
+    "name": "Name",
+    "photo" :"Phot.jpg",
+    "score": [
+       0,
+       0,
+       0,
+       0,
+       0,
+       0,
+       0,
+       0,
+       0,
+       0,
+   ]
+}
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tableArray;
